13ec43e7677a12f034b74d1df73a210c8513e291
macitege <macitegeercan@gmail.com>
2019-05-03 18:25:16 -0400

    Secondary Commit

--- app.js
+++ app.js
@@ -2,9 +2,6 @@ const app = require('express')();
 const http = require('http').Server(app);
 const io = require('socket.io')(http);
 
-
-// console.log(`________THIS IS APP:`, http)
-
 app.get('/', (req, res) => {
     res.sendFile(__dirname + '/index.html')
 })
